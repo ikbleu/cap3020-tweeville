d9a8b957c3e4c5a75c6855764222d6ae60bf45d3
Scandinavianblue <Scandinavianblue@d7fdeb4d-ad6f-fcb1-c464-be18161fb56b>
2010-03-26 16:46:29 +0000

    editing the basics of the controller

--- trunk/Control/BattleControl.java
+++ trunk/Control/BattleControl.java
@@ -5,12 +5,27 @@
 
 package Control;
 
-
+import java.awt.event.KeyEvent;
 
 /**
  *
  * @author spock
  */
-class BattleControl{
+class BattleControl extends GenAdapter{
+    private boolean active = false;
 
+    BattleControl(){
+        active = false;
+
+    }
+
+    void turnOn(){
+        active = true;
+    }
+
+    public void keyPressed(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_DOWN){
+            System.out.println("You pressed down!");
+        }
+    }
 }
