47dafc73bfe4c2f98958da249c40a76502db864f
wachickywa <wachickywa@d7fdeb4d-ad6f-fcb1-c464-be18161fb56b>
2010-03-26 18:34:46 +0000

    SplashControl updated and keys put in.

--- Control/SplashControl.java
+++ Control/SplashControl.java
@@ -12,10 +12,11 @@ import java.awt.event.KeyEvent;
  */
 public class SplashControl extends GenAdapter{
     private boolean active = false;
+    String list[] = { "Start", "LoadGame", "Exit" };
+    int selector = 0;
 
     SplashControl(){
         active = false;
-
     }
 
     void turnOn(){
@@ -25,6 +26,34 @@ public class SplashControl extends GenAdapter{
     public void keyPressed(KeyEvent e){
         if (e.getKeyCode() == KeyEvent.VK_DOWN){
             System.out.println("You pressed down!");
+            selector++;
+            if (selector == list.length){
+                selector = 0;
+            }
+        }
+
+        else if (e.getKeyCode() == KeyEvent.VK_UP){
+            System.out.println("You pressed up!");
+            selector--;
+            if (selector == -1){
+                selector = list.length - 1;
+            }
+        }
+
+        else if (e.getKeyCode() == KeyEvent.VK_ENTER){
+            System.out.println("You pressed enter!");
+            if (selector == 0){
+                System.out.println("Starting game!");
+            }
+
+            else if (selector == 1){
+                System.out.println("Loading the game!");
+            }
+
+            else if (selector == 2){
+                System.out.println("Exit!");
+                System.exit(0);
+            }
         }
     }
 }
