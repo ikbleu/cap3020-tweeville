b30465fa0fe859a82a9972c6f8eccd342217fc2c
ccrm88 <ccrm88@d7fdeb4d-ad6f-fcb1-c464-be18161fb56b>
2010-03-28 21:40:32 +0000

    added drawing capabilities to BattleScreen (so it can draw the BattleScreenHUD, at the moment)

--- trunk/View/BattleScreen.java
+++ trunk/View/BattleScreen.java
@@ -7,8 +7,35 @@ package View;
 
 /**
  *
- * @author spock
+ * @author chris
  */
-public class BattleScreen{
 
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+public class BattleScreen extends SpecialImage
+{
+    private int imageWidth, imageHeight;
+    private int hudPosX, hudPosY;
+    private BattleHUD hud;
+
+    BattleScreen()
+    {
+	imageWidth = 1280;
+	imageHeight = 800;
+	hudPosX = (int) (0.1875 * imageWidth);
+	hudPosY = (int) (0.85625 * imageHeight);
+	imageBuffer = new BufferedImage( imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB );
+
+	hud = new BattleHUD();
+    }
+
+    void refreshImage()
+    {
+	Graphics2D g2d = (Graphics2D) imageBuffer.getGraphics();
+	g2d.drawImage( graphics.getGraphic( ViewableEnums.BATTLESCREEN_VIEWPORT ), 0, 0, null );
+
+	hud.refreshImage();
+	g2d.drawImage( hud.image(), hudPosX, hudPosY, null );
+    }
 }
