f77df2580b4a5c8d4997d4eb4bb7a810b83641de
wachickywa <wachickywa@d7fdeb4d-ad6f-fcb1-c464-be18161fb56b>
2010-03-28 22:36:25 +0000

    Updated the BattleHUD.  Made the set methods and organize the HUD properly◘

--- trunk/View/BattleHUD.java
+++ trunk/View/BattleHUD.java
@@ -28,13 +28,17 @@ class BattleHUD extends SpecialImage {
     int melee[] = { 100, 100, 100 };
     int ranged[] = { 100, 100, 100 };
     Line2D lines[] = new Line2D.Double[9];
+    int list[] = { health[0], health[1], health[2], melee[0], melee[1], melee[2],
+        ranged[0], ranged[1], ranged[2] };
+    Color colors[] = { Color.green, Color.green, Color.green, Color.blue, Color.blue, Color.blue,
+        Color.blue, Color.blue, Color.blue };
 
     BattleHUD(){
         imageBuffer = new BufferedImage( WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB );
         g2 = imageBuffer.createGraphics();
-        hint = new RenderingHints( RenderingHints.KEY_TEXT_ANTIALIASING,
-            RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        font = new Font("Levenim MT", Font.PLAIN, 38);
+        hint = new RenderingHints( RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
+        font = new Font("Levenim MT", Font.PLAIN, 10);
         g2.setFont( font );
         g2.setRenderingHints( hint );
         for( int i = 0; i < lines.length; i++ ){
@@ -47,7 +51,9 @@ class BattleHUD extends SpecialImage {
         g2.setColor( new Color( 0xFFCDAA7D ) );
 	g2.fillRect( 0, 0, WIDTH, HEIGHT );
 
-        g2.drawImage( graphics.getGraphic(ViewableEnums.TESTHUDFACE), 20, 20, null );
+        g2.drawImage( graphics.getGraphic(ViewableEnums.TESTHUDFACE), (int)(WIDTH/41.5), (int)(HEIGHT/6.76), null );
+        g2.drawImage( graphics.getGraphic(ViewableEnums.TESTHUDFACE), (int)(WIDTH/2.86), (int)(HEIGHT/6.76), null );
+        g2.drawImage( graphics.getGraphic(ViewableEnums.TESTHUDFACE), (int)(WIDTH/1.48), (int)(HEIGHT/6.76), null );
         //g2.drawImage( graphics.getGraphic(ViewableEnums.TOITLETYMEHUDFACE), 20, 20, null );
         //g2.drawImage( graphics.getGraphic(ViewableEnums.NASTIEHUDFACE), 300, 20, null );
         //g2.drawImage( graphics.getGraphic(ViewableEnums.GUNDERSONHUDFACE), 600, 20, null );
@@ -56,30 +62,81 @@ class BattleHUD extends SpecialImage {
         //g2.drawImage( graphics.getGraphic(ViewableEnums.GUNDERSONHUDNAME), 650, 20, null );
 
         g2.setColor( Color.black );
-        g2.drawString( "HP:", 220, 50 );
-        g2.drawString( "HP:", 420, 50 );
-        g2.drawString( "HP:", 620, 50 );
-        g2.drawString( "Melee:", 220, 70 );
-        g2.drawString( "Melee:", 420, 70 );
-        g2.drawString( "Melee:", 620, 70 );
-        g2.drawString( "Ranged:", 220, 90 );
-        g2.drawString( "Ranged:", 420, 90 );
-        g2.drawString( "Ranged:", 620, 90 );
+        g2.drawString( "HP", (int)(WIDTH/7.54), (int)(HEIGHT/2.09) );
+        g2.drawString( "HP", (int)(WIDTH/2.18), (int)(HEIGHT/2.09) );
+        g2.drawString( "HP", (int)(WIDTH/1.2769), (int)(HEIGHT/2.09) );
+        g2.drawString( "MC", (int)(WIDTH/7.54), (int)(HEIGHT/1.53) );
+        g2.drawString( "MC", (int)(WIDTH/2.18), (int)(HEIGHT/1.53) );
+        g2.drawString( "MC", (int)(WIDTH/1.2769), (int)(HEIGHT/1.53) );
+        g2.drawString( "RC", (int)(WIDTH/7.54), (int)(HEIGHT/1.21) );
+        g2.drawString( "RC", (int)(WIDTH/2.18), (int)(HEIGHT/1.21) );
+        g2.drawString( "RC", (int)(WIDTH/1.2769), (int)(HEIGHT/1.21) );
 
-        g2.setStroke( new BasicStroke( 1.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND ) );
-        lines[0].setLine( 225,50, 225 + health[0], 50 );
-        lines[1].setLine( 425,50, 425 + health[1], 50 );
-        lines[2].setLine( 625,50, 625 + health[2], 50 );
-        lines[3].setLine( 225,70, 225 + melee[0], 70 );
-        lines[4].setLine( 425,70, 425 + melee[1], 70 );
-        lines[5].setLine( 625,70, 625 + melee[2], 70 );
-        lines[6].setLine( 225,90, 225 + ranged[0], 90 );
-        lines[7].setLine( 425,90, 425 + ranged[1], 90 );
-        lines[8].setLine( 625,90, 625 + ranged[2], 90 );
+        g2.setStroke( new BasicStroke( 5.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND ) );
+        lines[0].setLine( (int)(WIDTH/6.14), (int)(HEIGHT/2.3), (int)(WIDTH/6.14) + ((int)(WIDTH/6.38))*health[0]/100, (int)(HEIGHT/2.3) );
+        lines[1].setLine( (int)(WIDTH/2.049), (int)(HEIGHT/2.3), (int)(WIDTH/2.049) + ((int)(WIDTH/6.38))*health[1]/100, (int)(HEIGHT/2.3) );
+        lines[2].setLine( (int)(WIDTH/1.229), (int)(HEIGHT/2.3), (int)(WIDTH/1.229) + ((int)(WIDTH/6.38))*health[2]/100, (int)(HEIGHT/2.3) );
+        lines[3].setLine( (int)(WIDTH/6.14), (int)(HEIGHT/1.64), (int)(WIDTH/6.14) + ((int)(WIDTH/6.38))*melee[0]/100, (int)(HEIGHT/1.64) );
+        lines[4].setLine( (int)(WIDTH/2.049), (int)(HEIGHT/1.64), (int)(WIDTH/2.049) + ((int)(WIDTH/6.38))*melee[1]/100, (int)(HEIGHT/1.64) );
+        lines[5].setLine( (int)(WIDTH/1.229), (int)(HEIGHT/1.64), (int)(WIDTH/1.229) + ((int)(WIDTH/6.38))*melee[2]/100, (int)(HEIGHT/1.64) );
+        lines[6].setLine( (int)(WIDTH/6.14), (int)(HEIGHT/1.27), (int)(WIDTH/6.14) + ((int)(WIDTH/6.38))*ranged[0]/100, (int)(HEIGHT/1.27) );
+        lines[7].setLine( (int)(WIDTH/2.049), (int)(HEIGHT/1.27), (int)(WIDTH/2.049) + ((int)(WIDTH/6.38))*ranged[1]/100, (int)(HEIGHT/1.27) );
+        lines[8].setLine( (int)(WIDTH/1.229), (int)(HEIGHT/1.27), (int)(WIDTH/1.229) + ((int)(WIDTH/6.38))*ranged[2]/100, (int)(HEIGHT/1.27) );
 
-        g2.setColor( Color.blue );
         for( int i = 0; i < lines.length; i++ ){
-            g2.draw(lines[i]);
+            if (list[i] != 0 ){
+                if(list[i] < 20)
+                    g2.setColor(Color.red);
+                else
+                    g2.setColor(colors[i]);
+
+                g2.draw(lines[i]);
+            }
         }
+    }
+
+    void setHP0(int hp){
+        health[0] = hp;
+        list[0] = hp;
+    }
+
+    void setHP1(int hp){
+        health[1] = hp;
+        list[3] = hp;
+    }
+
+    void setHP2(int hp){
+        health[2] = hp;
+        list[6] = hp;
+    }
+
+    void setMC0(int mc){
+        melee[0] = mc;
+        list[1] = mc;
+    }
+
+    void setMC1(int mc){
+        melee[1] = mc;
+        list[4] = mc;
+    }
+
+    void setMC2(int mc){
+        melee[2] = mc;
+        list[7] = mc;
+    }
+
+    void setRC0(int rc){
+        ranged[0] = rc;
+        list[2] = rc;
+    }
+
+    void setRC1(int rc){
+        ranged[1] = rc;
+        list[5] = rc;
+    }
+
+    void setRC2(int rc){
+        ranged[2] = rc;
+        list[8] = rc;
     }
 }
