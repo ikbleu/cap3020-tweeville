2ae0136cc1b5a9288fb3cb0495c4c08e5d1de8e5
Scandinavianblue <Scandinavianblue@d7fdeb4d-ad6f-fcb1-c464-be18161fb56b>
2010-04-02 01:23:45 +0000

    added the cooldown interface

---
+++ trunk/Model/ViewCooldown.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author spock
+ */
+public interface ViewCooldown {
+    public abstract String getName();
+    public abstract int timeLeft();
+}
