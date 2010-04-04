c7ed9f984a6b893891af4e7a587982a3164c64bc
Scandinavianblue <Scandinavianblue@d7fdeb4d-ad6f-fcb1-c464-be18161fb56b>
2010-04-04 04:04:09 +0000

    committed a drawMe method

--- trunk/View/SpecialImage.java
+++ trunk/View/SpecialImage.java
@@ -5,6 +5,20 @@
 
 package View;
 
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLException;
+import javax.media.opengl.GLContext;
+import javax.media.opengl.GLDrawableFactory;
+import javax.swing.JFrame;
+import com.sun.opengl.util.FPSAnimator;
+import com.sun.opengl.util.ImageUtil;
+
+import com.sun.opengl.util.texture.Texture;
+
 /**
  *
  * @author spock
@@ -12,4 +26,33 @@ package View;
 abstract class SpecialImage extends CanIHazImage{
     GraphicsTableSingleton graphics = GraphicsTableSingleton.getInstance();
     OpenGraphicsTable ographics = OpenGraphicsTable.getInstance();
+
+
+    void drawMe(GL gl, Texture texture, float x, float y){
+
+        Texture current = texture;
+
+        gl.glPushMatrix();
+
+            gl.glBegin(GL.GL_POLYGON);
+
+                gl.glTexCoord2d(0.0, 0.0);
+                gl.glVertex2d(x,y);
+
+                gl.glTexCoord2d(0.0, 1.0);
+                gl.glVertex2d(x, y + ((float)(current.getHeight())/(float)800));
+
+                gl.glTexCoord2d(1.0, 1.0);
+                gl.glVertex2d(x + ((float)(current.getWidth())/(float)1280), y + ((float)(current.getHeight())/(float)800));
+
+                gl.glTexCoord2d(1.0, 0.0);
+                gl.glVertex2d(x + ((float)(current.getWidth())/(float)1280), y);
+
+
+             gl.glEnd();
+
+        gl.glPopMatrix();
+
+    }
+
 }
